3e58eb17b55b74a2f96dfbc064719a5d1a2afe4c
adhritvashishth <adhritv@gmail.com>
2026-06-04 10:07:25 +0530

    leson2

---
+++ activity 1.py
@@ -0,0 +1,12 @@
+#printing a statement
+print("welcome to the world of programing")
+
+#print a number
+print(234)
+
+#nextline after print
+print("hello python\n")
+#passing multiple agruements
+print("hello",99)
+#end arguement
+print("welcome to ",end="*")
---
+++ activity2.py
@@ -0,0 +1,9 @@
+num1=23455
+name="adhrit"
+print(num1)
+print(name)
+
+codingal="codingal"
+print(codingal)
+name=input("enter your name")
+print("hello ",name," welcome to codingal")
